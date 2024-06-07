ccda4e71fd065f274cde2d4177d75144c9dc2b7f
José Pablo Pavón Ferrero <josepablo.pavon@sofitec.aero>
2024-06-07 16:39:16 +0200

    rouying

---
+++ src/app/shared/pages/home-page/home-page.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-home-page',
+  templateUrl: './home-page.component.html',
+})
+export class HomePageComponent {
+
+
+
+ }
